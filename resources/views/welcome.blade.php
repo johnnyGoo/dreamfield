d0e6482eaa55f73deaf644e5236435be497b2e7b
Johnny <157781745@qq.com>
2017-12-02 01:20:11 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Demo
+                    My First Laravel App !
                 </div>
 
                 <div class="links">
