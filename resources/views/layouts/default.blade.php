bd6245006490d946b2821871534e013593f911ad
Johnny <157781745@qq.com>
2017-12-05 17:31:22 +0800

    added list and delete

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="content-language" content="zh-CN" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1,minimum-scale=1, maximum-scale=1">
     <title>@yield('title','dream field')</title>
