b02704ee0fd62b5627489b1f775db03b47b8b2e4
Johnny <157781745@qq.com>
2017-12-18 02:00:13 +0800

    finished

--- app/Http/Controllers/StatusesController.php
+++ app/Http/Controllers/StatusesController.php
@@ -16,6 +16,7 @@ class StatusesController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
     }
     public function store(Request $request)
     {
--- app/User.php
+++ app/User.php
@@ -6,18 +6,53 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 use App\Notifications\ResetPassword;
+use Auth;
 
-class User extends Authenticatable
-{ public function feed()
-{
-    return $this->statuses()
-        ->orderBy('created_at', 'desc');
-}
-    use Notifiable;
-    public function statuses()
+class User extends Authenticatable {
+    public function followers()
     {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+    public function follow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if (!is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
+    }
+
+
+    public function feed() {
+        $user_ids = Auth::user()->followings->pluck('id')->toArray();
+        array_push($user_ids, Auth::user()->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
+            ->orderBy('created_at', 'desc');
+    }
+
+    use Notifiable;
+
+    public function statuses() {
         return $this->hasMany(Status::class);
     }
+
 
     /**
      * The attributes that are mass assignable.
@@ -36,23 +71,20 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    public function gravatar($size = '100')
-    {
+
+    public function gravatar($size = '100') {
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
 
-    public static function boot()
-    {
+    public static function boot() {
         parent::boot();
-
         static::creating(function ($user) {
             $user->activation_token = str_random(30);
         });
     }
 
-    public function sendPasswordResetNotification($token)
-    {
+    public function sendPasswordResetNotification($token) {
         $this->notify(new ResetPassword($token));
     }
 
